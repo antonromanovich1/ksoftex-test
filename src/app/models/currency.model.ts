6642214a019a8704ec3868ca71c8848169ae15be
anton.ramanovich <a.ramanovich@andersenlab.com>
2025-04-26 15:52:44 +0200

    currency models

---
+++ src/app/models/currency.model.ts
@@ -0,0 +1,15 @@
+export interface Currency {
+  symbol: string;
+  name: string;
+  symbol_native: string;
+  decimal_digits: number;
+  rounding: number;
+  code: string;
+  name_plural: string;
+}
+
+export interface CurrencyResponse {
+  data: {
+    [key: string]: Currency;
+  };
+}
