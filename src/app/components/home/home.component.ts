5c065439127d91fe1819552127f8dbfdfec709c4
anton.ramanovich <a.ramanovich@andersenlab.com>
2025-04-26 23:37:17 +0200

    adds history track

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
-import { AsyncPipe } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { AsyncPipe, DatePipe } from '@angular/common';
+import { Component, inject, OnInit, signal, effect } from '@angular/core';
 import {
   FormControl,
   NonNullableFormBuilder,
@@ -11,8 +11,11 @@ import {
   IonContent,
   IonCard,
   IonCardContent,
+  IonList,
+  IonItem,
+  IonLabel,
 } from '@ionic/angular/standalone';
-import { shareReplay, Subject } from 'rxjs';
+import { shareReplay } from 'rxjs';
 import { CurrencyService } from 'src/app/services/currency.service';
 
 @Component({
@@ -26,15 +29,23 @@ import { CurrencyService } from 'src/app/services/currency.service';
     IonCardContent,
     AsyncPipe,
     ReactiveFormsModule,
+    IonList,
+    IonItem,
+    IonLabel,
+    DatePipe,
   ],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+    this.getHistory();
+  }
+
   private fb = inject(NonNullableFormBuilder);
   private currencyService = inject(CurrencyService);
 
-  protected convert$ = new Subject<void>();
-
   protected result$ = signal<number | null>(null);
+
+  history$ = signal<any[]>([]);
 
   form = this.fb.group({
     from: 'USD',
@@ -47,15 +58,17 @@ export class HomeComponent {
   currencyList$ = this.currencyService.getCurrencyList().pipe(shareReplay(1));
 
   triggerConvert() {
-    const { from, to, amount } = this.form.value;
+    const { from, to, amount } = this.form.getRawValue();
     if (from === to) {
       this.result$.set(amount || 0);
       return;
     } else {
       this.currencyService.getRate(from || '', to || '').subscribe((data) => {
         const rate = data[to || ''] as number;
-        const result = rate * (amount || 0);
-        this.result$.set(+result.toFixed(2));
+        const result = +(rate * (amount || 0)).toFixed(2);
+        this.result$.set(result);
+
+        this.saveToHistory(from, to, amount || 0, result, rate);
       });
     }
   }
@@ -63,5 +76,30 @@ export class HomeComponent {
   reverse() {
     const { from, to, amount } = this.form.value;
     this.form.patchValue({ from: to, to: from, amount });
+  }
+
+  private saveToHistory(
+    from: string,
+    to: string,
+    amount: number,
+    result: number,
+    rate: number
+  ) {
+    const history = JSON.parse(localStorage.getItem('history') || '[]');
+    history.unshift({
+      date: new Date(),
+      from,
+      to,
+      amount,
+      result,
+      rate,
+    });
+    localStorage.setItem('history', JSON.stringify(history));
+
+    this.getHistory();
+  }
+
+  private getHistory() {
+    this.history$.set(JSON.parse(localStorage.getItem('history') || '[]'));
   }
 }
